71a940c563991b68fe2924f88894eb72fe6aa7d2
albertoguerrero20 <m.alberto.guerrero.101@gmail.com>
2024-04-11 13:58:38 +0100

    cambios en casa

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import DatePicker from "../../components/datePicker/DatePicker";
 import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/header/Header";
+import Header from "../../components/header/Header";
 import Legacy from "../../components/legacy/Legacy";
 import Maps from "../../components/maps/Maps";
 import Section1 from "../../components/section1/Section1";
@@ -15,7 +15,7 @@ function Home() {
       <Section2 />
       <DatePicker />
       <Maps />
-      <Footer/>
+      <Footer />
     </>
   )
 
