728d66d1f7f95295b1fb5e63712133b65551d0ac
albertoguerrero20 <m.alberto.guerrero.101@gmail.com>
2024-05-16 22:05:11 +0100

    cookies change

--- src/components/cookies/Cookies.jsx
+++ src/components/cookies/Cookies.jsx
@@ -19,8 +19,10 @@ function Cookies() {
     <div className="cookies-wrapper" >
       <div className="cookies">
         <p>Utilizamos cookies propias y de terceros para conocer los usos de nuestra tienda online y poder mejorarla, adaptar el contenido a tus gustos y personalizar nuestros anuncios, marketing y publicaciones en redes sociales. Puedes aceptarlas todas, rechazarlas o elegir tu configuración pulsando los botones correspondientes. Ten en cuenta que rechazar las cookies puede afectar a tu experiencia de compra. Para más información puedes consultar nuestra <a className="cookies-Url" href="/cookies">Política de cookies</a></p>
-        <button className="cookies-btn" onClick={cookiesDenied}>Rechazar las cookies</button>
-        <button className="cookies-btn" onClick={cookiesAccepted}>Aceptar las cookies</button>
+        <div className="cookies-buttons">
+          <button className="cookies-btn" onClick={cookiesDenied}>Rechazar las cookies</button>
+          <button className="cookies-btn" onClick={cookiesAccepted}>Aceptar las cookies</button>
+        </div>
       </div>
     </div>
 
